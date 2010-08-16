f3b366873fb990575a417e4856370603bfc0532f
Kasima Tharnpipitchai <kasima@gmail.com>
2010-08-16 18:35:42 +0800

    Include user agent, parse Chrome 5+ stack trace

--- notifier.js
+++ notifier.js
@@ -2,7 +2,7 @@ var Hoptoad = {
   VERSION           : '0.1.0',
   NOTICE_XML        : '<?xml version="1.0" encoding="UTF-8"?><notice version="2.0"><api-key></api-key><notifier><name>hoptoad_notifier_js</name><version>0.1.0</version><url>http://hoptoadapp.com</url></notifier><error><class>EXCEPTION_CLASS</class><message>EXCEPTION_MESSAGE</message><backtrace>BACKTRACE_LINES</backtrace></error><request><url>REQUEST_URL</url><component>REQUEST_COMPONENT</component><action>REQUEST_ACTION</action></request><server-environment><project-root>PROJECT_ROOT</project-root><environment-name>production</environment-name></server-environment></notice>',
   ROOT              : window.location.protocol + '//' + window.location.host,
-  BACKTRACE_MATCHER : /^(.*)\@(.*)\:(\d+)$/,
+  BACKTRACE_MATCHER : /^(?:\s*at )?(.*)(?:\@| \()(.*)\:(\d+)(?:\:\d+\))*$/, // matches FF 3.5 and CHROME 5 stack traces
   backtrace_filters : [/notifier\.js/],
 
   notify: function(error) {
@@ -47,7 +47,8 @@ var Hoptoad = {
     var type      = Hoptoad.escapeText(error.type      || 'Error');
     var action    = Hoptoad.escapeText(error.action    || '');
     var message   = Hoptoad.escapeText(error.message   || 'Unknown error.');
-    var component = Hoptoad.escapeText(error.component || '');
+    var component = Hoptoad.escapeText(error.component || 'js');
+    var userAgent = Hoptoad.escapeText(navigator.userAgent || 'Unknown');
     var backtrace = Hoptoad.generateBacktrace(error);
 
     if (Hoptoad.trim(url) == '' && Hoptoad.trim(component) == '') {
@@ -55,6 +56,12 @@ var Hoptoad = {
     } else {
       var data    = '';
       var methods = ['params', 'session', 'cgi-data'];
+
+      // add user agent string
+      if (typeof(error['cgi-data']) == 'undefined') {
+          error['cgi-data'] = {};
+      }
+      error['cgi-data']['HTTP_USER_AGENT'] = userAgent;
 
       for (var i = 0; i < 3; i++) {
         var type = methods[i];
@@ -98,13 +105,12 @@ var Hoptoad = {
 
       if (matches && Hoptoad.validBacktraceLine(line)) {
         var file = matches[2].replace(Hoptoad.ROOT, '[PROJECT_ROOT]');
-
-        backtrace.push('<line method="' + Hoptoad.escapeText(matches[1]) +
+        var xmlLine = '<line method="' + Hoptoad.escapeText(matches[1]) +
                        '" file="' + Hoptoad.escapeText(file) +
-                       '" number="' + matches[3] + '" />');
+                       '" number="' + matches[3] + '" />';
+        backtrace.push(xmlLine);
       }
     }
-
     return backtrace;
   },
 
@@ -112,7 +118,7 @@ var Hoptoad = {
     var stacktrace = printStackTrace({ e : error, guess : false });
 
     for (var i = 0, l = stacktrace.length; i < l; i++) {
-      if (stacktrace[i].match(/\:\d+$/)) {
+      if (stacktrace[i].match(/\:\d+\)?$/)) {
         continue;
       }
 
@@ -122,7 +128,6 @@ var Hoptoad = {
 
       stacktrace[i] += ':0';
     }
-
     return stacktrace;
   },
 
@@ -211,14 +216,143 @@ var Hoptoad = {
 // DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
 // IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
 // OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-function printStackTrace(a){var b=a&&a.e?a.e:null;a=a?!!a.guess:true;var c=new printStackTrace.implementation;b=c.run(b);return a?c.guessFunctions(b):b}printStackTrace.implementation=function(){};
-printStackTrace.implementation.prototype={run:function(a){var b=this._mode||this.mode();if(b==="other")return this.other(arguments.callee);else{var c;if(!(c=a))a:{try{0()}catch(d){c=d;break a}c=void 0}a=c;return this[b](a)}},mode:function(){try{0()}catch(a){if(a.arguments)return this._mode="chrome";else if(a.stack)return this._mode="firefox";else if(window.opera&&!("stacktrace"in a))return this._mode="opera"}return this._mode="other"},chrome:function(a){return a.stack.replace(/^.*?\n/,"").replace(/^.*?\n/,
-"").replace(/^.*?\n/,"").replace(/^[^\(]+?[\n$]/gm,"").replace(/^\s+at\s+/gm,"").replace(/^Object.<anonymous>\s*\(/gm,"{anonymous}()@").split("\n")},firefox:function(a){return a.stack.replace(/^.*?\n/,"").replace(/(?:\n@:0)?\s+$/m,"").replace(/^\(/gm,"{anonymous}(").split("\n")},opera:function(a){a=a.message.split("\n");var b=/Line\s+(\d+).*?script\s+(http\S+)(?:.*?in\s+function\s+(\S+))?/i,c,d,e;c=4;d=0;for(e=a.length;c<e;c+=2)if(b.test(a[c]))a[d++]=(RegExp.$3?RegExp.$3+"()@"+RegExp.$2+RegExp.$1:
-"{anonymous}()@"+RegExp.$2+":"+RegExp.$1)+" -- "+a[c+1].replace(/^\s+/,"");a.splice(d,a.length-d);return a},other:function(a){for(var b=/function\s*([\w\-$]+)?\s*\(/i,c=[],d=0,e,f;a&&c.length<10;){e=b.test(a.toString())?RegExp.$1||"{anonymous}":"{anonymous}";f=Array.prototype.slice.call(a.arguments);c[d++]=e+"("+printStackTrace.implementation.prototype.stringifyArguments(f)+")";if(a===a.caller&&window.opera)break;a=a.caller}return c},stringifyArguments:function(a){for(var b=0;b<a.length;++b){var c=
-a[b];if(typeof c=="object")a[b]="#object";else if(typeof c=="function")a[b]="#function";else if(typeof c=="string")a[b]='"'+c+'"'}return a.join(",")},sourceCache:{},ajax:function(a){var b=this.createXMLHTTPObject();if(b){b.open("GET",a,false);b.setRequestHeader("User-Agent","XMLHTTP/1.0");b.send("");return b.responseText}},createXMLHTTPObject:function(){for(var a,b=[function(){return new XMLHttpRequest},function(){return new ActiveXObject("Msxml2.XMLHTTP")},function(){return new ActiveXObject("Msxml3.XMLHTTP")},
-function(){return new ActiveXObject("Microsoft.XMLHTTP")}],c=0;c<b.length;c++)try{a=b[c]();this.createXMLHTTPObject=b[c];return a}catch(d){}},getSource:function(a){a in this.sourceCache||(this.sourceCache[a]=this.ajax(a).split("\n"));return this.sourceCache[a]},guessFunctions:function(a){for(var b=0;b<a.length;++b){var c=a[b],d=/{anonymous}\(.*\)@(\w+:\/\/([-\w\.]+)+(:\d+)?[^:]+):(\d+):?(\d+)?/.exec(c);if(d){var e=d[1];d=d[4];if(e&&d){e=this.guessFunctionName(e,d);a[b]=c.replace("{anonymous}",e)}}}return a},
-guessFunctionName:function(a,b){try{return this.guessFunctionNameFromLines(b,this.getSource(a))}catch(c){return"getSource failed with url: "+a+", exception: "+c.toString()}},guessFunctionNameFromLines:function(a,b){for(var c=/function ([^(]*)\(([^)]*)\)/,d=/['"]?([0-9A-Za-z_]+)['"]?\s*[:=]\s*(function|eval|new Function)/,e="",f=0;f<10;++f){e=b[a-f]+e;if(e!==undefined){var g=d.exec(e);if(g&&g[1])return g[1];else if((g=c.exec(e))&&g[1])return g[1]}}return"(?)"}};
-
+function printStackTrace(a) {
+    var b = a && a.e ? a.e : null;
+    a = a ? !! a.guess : true;
+    var c = new printStackTrace.implementation;
+    b = c.run(b);
+    return a ? c.guessFunctions(b) : b
+}
+printStackTrace.implementation = function () {};
+printStackTrace.implementation.prototype = {
+    run: function (a) {
+        var b = this._mode || this.mode();
+        if (b === "other") return this.other(arguments.callee);
+        else {
+            var c;
+            if (!(c = a)) a: {
+                try {
+                    0()
+                } catch (d) {
+                    c = d;
+                    break a
+                }
+                c = void 0
+            }
+            a = c;
+            return this[b](a)
+        }
+    },
+    mode: function () {
+        try {
+            0()
+        } catch (a) {
+            // Changed to support Chrome 5+.  If there's a stack attribute, use it.  -KT
+            if (a.stack) return this._mode = "firefox";
+            else if (a.arguments) return this._mode = "chrome";
+            else if (window.opera && !("stacktrace" in a)) return this._mode = "opera"
+        }
+        return this._mode = "other"
+    },
+    chrome: function (a) {
+        return a.stack.replace(/^.*?\n/, "").replace(/^.*?\n/, "").replace(/^.*?\n/, "").replace(/^[^\(]+?[\n$]/gm, "").replace(/^\s+at\s+/gm, "").replace(/^Object.<anonymous>\s*\(/gm, "{anonymous}()@").split("\n")
+    },
+    firefox: function (a) {
+        return a.stack.replace(/^.*?\n/, "").replace(/(?:\n@:0)?\s+$/m, "").replace(/^\(/gm, "{anonymous}(").split("\n")
+    },
+    opera: function (a) {
+        a = a.message.split("\n");
+        var b = /Line\s+(\d+).*?script\s+(http\S+)(?:.*?in\s+function\s+(\S+))?/i,
+            c, d, e;
+        c = 4;
+        d = 0;
+        for (e = a.length; c < e; c += 2) if (b.test(a[c])) a[d++] = (RegExp.$3 ? RegExp.$3 + "()@" + RegExp.$2 + RegExp.$1 : "{anonymous}()@" + RegExp.$2 + ":" + RegExp.$1) + " -- " + a[c + 1].replace(/^\s+/, "");
+        a.splice(d, a.length - d);
+        return a
+    },
+    other: function (a) {
+        for (var b = /function\s*([\w\-$]+)?\s*\(/i, c = [], d = 0, e, f; a && c.length < 10;) {
+            e = b.test(a.toString()) ? RegExp.$1 || "{anonymous}" : "{anonymous}";
+            f = Array.prototype.slice.call(a.arguments);
+            c[d++] = e + "(" + printStackTrace.implementation.prototype.stringifyArguments(f) + ")";
+            if (a === a.caller && window.opera) break;
+            a = a.caller
+        }
+        return c
+    },
+    stringifyArguments: function (a) {
+        for (var b = 0; b < a.length; ++b) {
+            var c =
+            a[b];
+            if (typeof c == "object") a[b] = "#object";
+            else if (typeof c == "function") a[b] = "#function";
+            else if (typeof c == "string") a[b] = '"' + c + '"'
+        }
+        return a.join(",")
+    },
+    sourceCache: {},
+    ajax: function (a) {
+        var b = this.createXMLHTTPObject();
+        if (b) {
+            b.open("GET", a, false);
+            b.setRequestHeader("User-Agent", "XMLHTTP/1.0");
+            b.send("");
+            return b.responseText
+        }
+    },
+    createXMLHTTPObject: function () {
+        for (var a, b = [function () {
+            return new XMLHttpRequest
+        }, function () {
+            return new ActiveXObject("Msxml2.XMLHTTP")
+        }, function () {
+            return new ActiveXObject("Msxml3.XMLHTTP")
+        }, function () {
+            return new ActiveXObject("Microsoft.XMLHTTP")
+        }], c = 0; c < b.length; c++) try {
+            a = b[c]();
+            this.createXMLHTTPObject = b[c];
+            return a
+        } catch (d) {}
+    },
+    getSource: function (a) {
+        a in this.sourceCache || (this.sourceCache[a] = this.ajax(a).split("\n"));
+        return this.sourceCache[a]
+    },
+    guessFunctions: function (a) {
+        for (var b = 0; b < a.length; ++b) {
+            var c = a[b],
+                d = /{anonymous}\(.*\)@(\w+:\/\/([-\w\.]+)+(:\d+)?[^:]+):(\d+):?(\d+)?/.exec(c);
+            if (d) {
+                var e = d[1];
+                d = d[4];
+                if (e && d) {
+                    e = this.guessFunctionName(e, d);
+                    a[b] = c.replace("{anonymous}", e)
+                }
+            }
+        }
+        return a
+    },
+    guessFunctionName: function (a, b) {
+        try {
+            return this.guessFunctionNameFromLines(b, this.getSource(a))
+        } catch (c) {
+            return "getSource failed with url: " + a + ", exception: " + c.toString()
+        }
+    },
+    guessFunctionNameFromLines: function (a, b) {
+        for (var c = /function ([^(]*)\(([^)]*)\)/, d = /['"]?([0-9A-Za-z_]+)['"]?\s*[:=]\s*(function|eval|new Function)/, e = "", f = 0; f < 10; ++f) {
+            e = b[a - f] + e;
+            if (e !== undefined) {
+                var g = d.exec(e);
+                if (g && g[1]) return g[1];
+                else if ((g = c.exec(e)) && g[1]) return g[1]
+            }
+        }
+        return "(?)"
+    }
+};
 
 
 
